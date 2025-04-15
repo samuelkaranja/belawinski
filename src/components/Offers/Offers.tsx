60f4deaceae24655f20faf1bbbfdb6c4311f4c6c
samuelkaranja <samuelkaranja007@gmail.com>
2025-04-15 21:53:29 +0300

    Implemented the Offers Component

---
+++ src/components/Offers/Offers.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./offers.css";
+
+const Offers: React.FC = () => {
+  return (
+    <div className="offers">
+      <div className="opening">
+        <h4>Open hours</h4>
+        <div className="list">
+          <div className="item">
+            <span>Monday - Friday</span>
+            <span>4:00 pm - 11:00 pm</span>
+          </div>
+          <div className="item">
+            <span>Saturday:</span>
+            <span>2:00 pm - 11:00 pm</span>
+          </div>
+          <div className="item">
+            <span>Sunday:</span>
+            <span>2:00 pm - 11:00 pm</span>
+          </div>
+        </div>
+        <h5>Visit Us</h5>
+      </div>
+      <div className="image"></div>
+      <div className="happyhour">
+        <h4>Happy hour</h4>
+        <div className="list">
+          <div className="item">
+            <span>Monday - Friday</span>
+            <span>4:00 pm - 7:00 pm</span>
+          </div>
+
+          <div className="item">
+            <span>20% off all wine by the glass</span>
+            <span>15% off all cocktails</span>
+          </div>
+
+          <div className="item">
+            <span>25% off selected craft beer</span>
+            <span>10% off all spirits</span>
+          </div>
+        </div>
+        <h5>Visit Us</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Offers;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./home.css";
 import Banner from "../../components/Banner/Banner";
+import Offers from "../../components/Offers/Offers";
 
 const Home: React.FC = () => {
   return (
     <div className="home">
       <Banner subtitle={"Exclusive drink service"} title={"Belawinski Bar"} />
+      <Offers />
     </div>
   );
 };
